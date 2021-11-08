6d00bf40d3bdba40a4366b8d96eea3dedbbc3084
ssk1162 <82650238+ssk1162@users.noreply.github.com>
2021-11-08 22:57:59 +0900

    21.11.08 22:57 setup ss

---
+++ Academy_project_4_iOS/ListController.swift
@@ -0,0 +1,18 @@
+//
+//  ListController.swift
+//  Academy_project_4_iOS
+//
+//  Created by kwonsoonsun on 2021/11/08.
+//
+
+import UIKit
+
+class ListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
---
+++ Academy_project_4_iOS/UserController.swift
@@ -0,0 +1,17 @@
+//
+//  UserController.swift
+//  Academy_project_4_iOS
+//
+//  Created by kwonsoonsun on 2021/11/08.
+//
+
+import UIKit
+
+class UserController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
