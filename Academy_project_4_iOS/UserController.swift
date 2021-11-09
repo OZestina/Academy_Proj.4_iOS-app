e2e893bef7b1850cae63b3c09eaf6e0e364b6102
ssk1162 <82650238+ssk1162@users.noreply.github.com>
2021-11-09 23:54:26 +0900

    21.11.09 23:54 setup-ss

--- Academy_project_4_iOS/UserController.swift
+++ Academy_project_4_iOS/UserController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SQLite3
 
 class UserController: UIViewController {
 
@@ -35,6 +36,12 @@ class UserController: UIViewController {
         }
         self.present(go, animated: true, completion: nil)
         
+        // 회원 db 생성
+//        let db = SQLite3DB()
+//        print("생성된 path는 ", db.path)
+//        db.createTable()
+        
+        
     }
     
     
@@ -44,4 +51,66 @@ class UserController: UIViewController {
         }
         self.present(go, animated: true, completion: nil)
     }
+}
+
+class SQLite3DB {
+    let path: String = {
+              let fm = FileManager.default
+              return fm.urls(for:.libraryDirectory, in:.userDomainMask).last!
+                       .appendingPathComponent("user.db").path
+        }()
+    
+    func openDatabase() -> OpaquePointer? {
+        var con : OpaquePointer? = nil
+        if sqlite3_open(path, &con) == SQLITE_OK {
+            print("연결 성공")
+        } else {
+            print("연결 실패")
+        }
+        return con
+    }
+    
+    
+    // 극장 테이블 생성
+//    let sql_create =
+//    """
+//create table users (
+//id int primary key not null,
+//title varchar(100) not null,
+//director varchar(100) not null,
+//actor varchar(100) not null,
+//time varchar(100) not null,
+//genre varchar(100) not null,
+//date varchar(100) not null,
+//imgFile varchar(100) not null
+//);
+//"""
+    
+    // 회원 테이블 생성
+//    let sql_create =
+//    """
+//create table users (
+//id char(255) primary key not null,
+//pw char(255) not null,
+//phone char(255) not null,
+//name char(255) not null
+//);
+//"""
+    
+    func createTable() {
+        let db = openDatabase()
+        var con : OpaquePointer? = nil
+        if sqlite3_prepare_v2(db, sql_create, -1, &con, nil) == SQLITE_OK {
+            print("sql문 객체화 성공")
+            if sqlite3_step(con) == SQLITE_DONE {
+                print("테이블 생성 성공")
+            } else {
+                print("테이블 생성 실패")
+            }
+        } else {
+            print("sql문 객체화 실패")
+        }
+        sqlite3_finalize(con)
+    }
+    
 }
