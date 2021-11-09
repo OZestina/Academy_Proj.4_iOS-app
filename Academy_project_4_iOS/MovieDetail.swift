0fcef8b1f60769047df607f8532255d8c4d19d94
ozestina <zestina.oh@gmail.com>
2021-11-09 18:42:06 +0900

    21.11.09 18:40 - oz

---
+++ Academy_project_4_iOS/MovieDetail.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetail.swift
+//  Academy_project_4_iOS
+//
+//  Created by Apple on 2021/11/09.
+//
+
+import UIKit
+
+class MovieDetail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Academy_project_4_iOS/Webpage.swift
@@ -0,0 +1,30 @@
+//
+//  Webpage.swift
+//  Academy_project_4_iOS
+//
+//  Created by Apple on 2021/11/09.
+//
+
+import UIKit
+import WebKit
+
+class Webpage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
