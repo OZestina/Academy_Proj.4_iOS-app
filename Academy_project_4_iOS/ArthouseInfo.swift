585828e95b4b90ff3b88abe5497d966aeaaa8422
ozestina <zestina.oh@gmail.com>
2021-11-14 00:05:42 +0900

    21.11.14 00:00 - oz

--- Academy_project_4_iOS/ArthouseInfo.swift
+++ Academy_project_4_iOS/ArthouseInfo.swift
@@ -6,16 +6,41 @@
 //
 
 import UIKit
+//MapView 사용을 위해 MapKit import 필요
+import MapKit
 
-class ArthouseInfo: UIViewController {
+//지도를 보여주기 위해 추가 delegate 선언
+class ArthouseInfo: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //영화관 좌표 입력 및 타이틀 설정
+        //37.56155949849524, 126.94706912625382
+        movieLocation(latitude: 37.56155949, longitude: 126.94706912, delta: 0.03, title: "아트하우스 모모")
     }
     
-
+    //위도, 경도, 범위를 파라미터로 받아 지도에 표시하는 함수 설정
+    func movieLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, delta span: Double, title strTitle: String) {
+        //위도, 경도 설정
+        let pLocation = CLLocationCoordinate2DMake(latitude, longitude)
+        //지도 표시 범위 설정
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        mapView.setRegion(pRegion, animated: true)
+        //어노테이션 객체 생성 & 위치 및 타이틀 설정
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = pLocation
+        annotation.title = strTitle
+        //맵 뷰에 표시
+        mapView.addAnnotation(annotation)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
